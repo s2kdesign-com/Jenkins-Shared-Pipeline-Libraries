e15744af4f62f5ed275d57ec3b54a98d257d3aae
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2019-06-29 13:11:27 +0300

    Refactored code

--- vars/get_version.groovy
+++ vars/get_version.groovy
@@ -2,23 +2,33 @@ import org.s2kdesign.IStepExecutor
 import org.s2kdesign.ioc.ContextRegistry
 
 /**
- * Getting default version depending on the branch. Versions are configured in resource folder 
+ * Getting default version depending on the branch. Versions are configured in resource folder
  * @param branchName Use GIT_BRANCH for Multibranch Pipeline Job
  * @return
  */
 def call(String branchName) {
     ContextRegistry.registerDefaultContext(this)
 
-    def versionMaster = libraryResource 'master_version';
-    def projectVersion = versionMaster.split('\\.');
+    def versionMaster = libraryResource 'master_version'
+    def versionDevelop = libraryResource 'develop_version'
+    def projectVersion = versionMaster.split('\\.')
 
     if (branchName == "master"){
-        return "${projectVersion[0]}.${projectVersion[1]}.${BUILD_NUMBER}.0"
+        def returnVersion = "${projectVersion[0]}.${projectVersion[1]}.${BUILD_NUMBER}.0"
+
+        echo  "using version ${returnVersion}"
+        return returnVersion
     } else if (branchName == "develop") {
-        def versionDevelop = libraryResource 'develop_version';
-        projectVersion = versionDevelop.split('\\.');
-        return "${projectVersion[0]}.${projectVersion[1]}.${BUILD_NUMBER}.0"
+        projectVersion = versionDevelop.split('\\.')
+
+        def returnVersion = "${projectVersion[0]}.${projectVersion[1]}.${BUILD_NUMBER}.0"
+
+        echo  "using version ${returnVersion}"
+        return returnVersion
     } else {
-        return "${projectVersion[0]}.${projectVersion[1]}.${projectVersion[2]}.${BUILD_NUMBER}"
+        def returnVersion = "${projectVersion[0]}.${projectVersion[1]}.${projectVersion[2]}.${BUILD_NUMBER}"
+
+        echo  "using version ${returnVersion}"
+        return returnVersion
     }
 }
