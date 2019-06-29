4c9ce900a0e5cccb7f53f05e1820d05e0bb31224
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2019-06-29 11:56:43 +0300

    added Get version method

---
+++ vars/get_version.groovy
@@ -0,0 +1,23 @@
+import org.s2kdesign.ioc.ContextRegistry
+
+/**
+ * Example custom step for easy use of MsBuild inside Jenkinsfiles
+ * @param solutionPath Path to .sln file
+ * @return
+ */
+def call(String branchName) {
+    ContextRegistry.registerDefaultContext(this)
+
+    def versionMaster = libraryResource 'MASTER_VERSION';
+    def versionDevelop = libraryResource 'DEVELOP_VERSION';
+
+    def projectVersion = versionMaster.split('\\.');
+
+    if (branchName == "master"){
+        return "${projectVersion[0]}.${projectVersion[1]}.${env.BUILD_NUMBER}.0"
+    } else if (branchName == "develop") {
+        return "${projectVersion[0]}.${projectVersion[1]}.${env.BUILD_NUMBER}.0"
+    } else {
+        return "${projectVersion[0]}.${projectVersion[1]}.${projectVersion[2]}.${env.BUILD_NUMBER}"
+    }
+}
