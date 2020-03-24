4028f6cb51a13b2b673623c669fdc90fb05dae43
Svetlin Krastanov <skrastanov@profitstars.com>
2020-03-24 16:48:37 +0200

    currentBuild set display name

--- vars/get_version.groovy
+++ vars/get_version.groovy
@@ -38,7 +38,8 @@ def call(String branchName) {
 
     echo  "Building version ${returnVersion}"
 
-    currentBuild.rawBuild.project.setDisplayName("${returnVersion}")
+    //currentBuild.rawBuild.project.setDisplayName("${returnVersion}")
+    currentBuild.displayName = "${returnVersion}"
     return returnVersion
 }
 
