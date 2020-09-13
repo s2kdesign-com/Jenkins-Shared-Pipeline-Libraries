d4397001baae15149402ec8159070a15e251f2ec
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2020-09-13 16:21:30 +0300

    Reversed Server Side Blazor

--- docs/app/Program.cs
+++ docs/app/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace JenkinsProjectVersions.Client
+namespace ProjectVersionsDemo
 {
     public class Program
     {
