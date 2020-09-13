d4dc513e15c0d0c6d6a6bc441beb341c4aa53649
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2020-09-13 15:52:13 +0300

    Changed to ServerSide blazor

--- docs/app/Client/Program.cs
+++ docs/app/Client/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace ProjectVersionsDemo
+namespace JenkinsProjectVersions.Client
 {
     public class Program
     {
